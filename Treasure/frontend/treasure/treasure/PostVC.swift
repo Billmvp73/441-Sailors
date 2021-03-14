04c1f1e386d612682a04feec3becb8134e957617
xinyun <xinyun@umich.edu>
2021-03-13 19:04:58 -0500

    show game

--- Treasure/frontend/treasure/treasure/PostVC.swift
+++ Treasure/frontend/treasure/treasure/PostVC.swift
@@ -17,7 +17,7 @@ class PostVC: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var tagTextView: UITextField!
     @IBAction func submitGames(_ sender: Any) {
-        let game = GamePost(userid: "change to google user id -- TODO",
+        let game = GamePost(username: "change to google user id -- TODO",
                         name: self.nameTextField.text, description: self.descriptionTextView.text, tag: self.tagTextView.text, puzzles: Array())
 //        let store = GamesStore()
 //        store.postGames(game)
