a5ec2a3b399a26e351b1cc8ec78d97dc4489449f
Pengyuan Huang <pyhuang@umich.edu>
2021-03-16 22:58:11 -0400

    Update PuzzlesVC.swift

--- Treasure/frontend/treasure/treasure/PuzzlesVC.swift
+++ Treasure/frontend/treasure/treasure/PuzzlesVC.swift
@@ -28,9 +28,8 @@ class PuzzlesVC: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate
     @IBOutlet weak var mMap: GMSMapView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var descriptionText: UITextView!
-    var game: GamePost? = nil
     @IBOutlet weak var nameText: UITextField!
-    var games: [GamePost]? = nil
+    var prevPuzzles: [Puzzle]? = nil
     var geodata: GeoData? = nil
     var puzzle: Puzzle? = nil
     var markerPress: [GMSMarker]? = nil
@@ -65,16 +64,16 @@ class PuzzlesVC: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate
         mMap.isMyLocationEnabled = true
         // enable the location bull's eye button
         mMap.settings.myLocationButton = true
-        var chattMarker: GMSMarker!
-            
-            if let geoLoc = geodata{
-                let coordinate = CLLocationCoordinate2D(latitude: geoLoc.lat, longitude: geoLoc.lon)
-                chattMarker = GMSMarker(position: coordinate)
-                chattMarker.map = mMap
-                puzzle = Puzzle(location: geoLoc, name: "setup a new game", type: "Empty", description: nil)
-                chattMarker.userData = puzzle
-                mMap.camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15.0)
-            }
+//        var chattMarker: GMSMarker!
+//            
+//            if let geoLoc = geodata{
+//                let coordinate = CLLocationCoordinate2D(latitude: geoLoc.lat, longitude: geoLoc.lon)
+//                chattMarker = GMSMarker(position: coordinate)
+//                chattMarker.map = mMap
+//                puzzle = Puzzle(location: geoLoc, name: "New Puzzle?", type: "Empty", description: nil)
+//                chattMarker.userData = puzzle
+//                mMap.camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15.0)
+//            }
 //        }
         
         locmanager.delegate = self
@@ -171,45 +170,21 @@ class PuzzlesVC: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate
             description.textColor = .darkGray
             view.addSubview(description)
         
-            
-           
-//           let timestamp = UILabel(frame: CGRect.init(x: 10, y: 10, width: view.frame.size.width - 16, height: 15))
-//           timestamp.text = game.timestamp
-//           timestamp.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-//           timestamp.textColor = .systemBlue
-//           view.addSubview(timestamp)
-//
-//           let username = UILabel(frame: CGRect.init(x: timestamp.frame.origin.x, y: timestamp.frame.origin.y + timestamp.frame.size.height + 5, width: view.frame.size.width - 16, height: 15))
-//           username.text = game.username
-//           username.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-//           username.textColor = .black
-//           view.addSubview(username)
-//
-//           let gamename = UILabel(frame: CGRect.init(x: timestamp.frame.origin.x, y: timestamp.frame.origin.y + timestamp.frame.size.height + 5, width: view.frame.size.width - 16, height: 15))
-//            gamename.text = game.gamename
-//            username.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-//            username.textColor = .black
-//            view.addSubview(username)
-//
-//           let description = UILabel(frame: CGRect.init(x: username.frame.origin.x, y: username.frame.origin.y + username.frame.size.height + 10, width: view.frame.size.width - 16, height: 15))
-//           description.text = game.description
-//           description.textColor = .darkGray
-//           view.addSubview(description)
 
            guard let geodata = puzzle.location else {
                return view
            }
 
-//           let infoLabel = UILabel(frame: CGRect.init(x: description.frame.origin.x, y: description.frame.origin.y + description.frame.size.height + 30, width: view.frame.size.width - 16, height: 40))
-//           infoLabel.text = "Posted from " + geodata.loc + ", while facing " + geodata.facing + " moving at " + geodata.speed + " speed."
-//           infoLabel.font = UIFont.systemFont(ofSize: 16)
-//
-//           infoLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
-//           infoLabel.numberOfLines = 2
-//
-//           infoLabel.textColor = .black
-//           infoLabel.highlight(searchedText: geodata.loc, geodata.facing, geodata.speed)
-//           view.addSubview(infoLabel)
+           let infoLabel = UILabel(frame: CGRect.init(x: description.frame.origin.x, y: description.frame.origin.y + description.frame.size.height + 30, width: view.frame.size.width - 16, height: 40))
+           infoLabel.text = "Posted from " + geodata.loc + "."
+           infoLabel.font = UIFont.systemFont(ofSize: 16)
+
+           infoLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+           infoLabel.numberOfLines = 2
+
+           infoLabel.textColor = .black
+           infoLabel.highlight(searchedText: geodata.loc, geodata.facing, geodata.speed)
+           view.addSubview(infoLabel)
            
            return view
        }
