c2fb8f7e09b691e41fa5354248da65e8b990fa49
xinyun <xinyun@umich.edu>
2021-03-25 15:50:42 -0400

    add ar model

--- Treasure/frontend/treasure/treasure/PuzzlesVC.swift
+++ Treasure/frontend/treasure/treasure/PuzzlesVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import GoogleMaps
+import SceneKit
 
 protocol ReturnDelegate: UIViewController {
     func onReturn(_ result: Puzzle)
@@ -21,6 +22,8 @@ class PuzzlesVC: UIViewController, UITextViewDelegate, CLLocationManagerDelegate
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var puzzletypeText: UITextField!
     @IBOutlet weak var puzzletypeDropdown: UIPickerView!
+    
+    @IBOutlet weak var sceneView: SCNView!
     
     //create puzzle list
     var list = ["word puzzle","interaction puzzle"]
@@ -77,6 +80,50 @@ class PuzzlesVC: UIViewController, UITextViewDelegate, CLLocationManagerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // 1: Load .obj file
+        let scene = SCNScene(named: "toy_biplane.usdz")
+        
+        // 2: Add camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        // 3: Place camera
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        // 4: Set camera on scene
+        scene?.rootNode.addChildNode(cameraNode)
+        
+        // 5: Adding light to scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        scene?.rootNode.addChildNode(lightNode)
+        
+        // 6: Creating and adding ambien light to scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene?.rootNode.addChildNode(ambientLightNode)
+                
+        // If you don't want to fix manually the lights
+    //        sceneView.autoenablesDefaultLighting = true
+        
+        // Allow user to manipulate camera
+        sceneView.allowsCameraControl = true
+        
+        // Show FPS logs and timming
+        // sceneView.showsStatistics = true
+        
+        // Set background color
+        sceneView.backgroundColor = UIColor.white
+        
+        // Allow user translate image
+        sceneView.cameraControlConfiguration.allowsTranslation = false
+        
+        // Set scene settings
+        sceneView.scene = scene
 //        scrollView.delegate = self
 //        puzzletypeText.delegate = self
 //        puzzletypeDropdown.delegate = self
