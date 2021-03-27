370c21c632bc42357292092da9c0c26165330bb5
Guoxin Yin <guoxiny@umich.edu>
2021-03-27 18:41:48 +0800

    update

---
+++ Treasure/frontend/treasure/treasure/GameInfo.swift
@@ -0,0 +1,16 @@
+//
+//  GameInfo.swift
+//  treasure
+//
+//  Created by Guoxin YIN on 2021/3/23.
+//
+
+import UIKit
+
+final class GameInfo: UIViewController {
+    override func viewDidLoad() {
+        <#code#>
+    }
+    
+    
+}
