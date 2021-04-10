1692550645d9446b9dac21101fc60751b3725099
潘芝亦 <pzhiyi@ip-10-212-133-31.cn-north-1.compute.internal>
2021-04-10 13:47:10 +0800

    street view

--- Treasure/frontend/treasure/treasure/PuzzlesVC.swift
+++ Treasure/frontend/treasure/treasure/PuzzlesVC.swift
@@ -522,7 +522,14 @@ class PuzzlesVC: UIViewController, UITextViewDelegate, CLLocationManagerDelegate
     
     var counterMarker: Int = 0
 
-
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) {
+        let storyBoard = UIStoryboard(name: "Main", bundle:nil)
+        if let streetViewController = storyBoard.instantiateViewController(withIdentifier: "StreetViewVC") as? StreetViewVC {
+          streetViewController.coordinate = marker.position
+          self.navigationController!.pushViewController(streetViewController, animated: true)
+        }
+      }
+    
     func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
 
         if self.counterMarker < 1
